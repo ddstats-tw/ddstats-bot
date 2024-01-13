2aead194f53b7932597ec6c1f83eaef17b5c3082
furo <urof@disroot.org>
2024-01-13 16:35:53 +0100

    Change `record_maps` to `maps`

--- events/commands/playtime.js
+++ events/commands/playtime.js
@@ -21,7 +21,7 @@ const invoke = async (interaction) => {
     await interaction.deferReply()
     const mostPlayedMaps = master.prepare("SELECT Map, SUM(time)/60/60 FROM record_snapshot WHERE name = ? GROUP BY map ORDER BY sum(time) DESC LIMIT 15").all(player)
     const totalPlaytime = master.prepare("SELECT SUM(time)/60/60 FROM record_snapshot WHERE name = ?").all(player)
-    const mostPlayedCategories = master.prepare("SELECT b.Server, SUM(a.time)/60/60 FROM record_snapshot AS a JOIN record_maps AS b ON a.map = b.map WHERE name = ? GROUP BY b.Server ORDER BY SUM(a.time) DESC").all(player)
+    const mostPlayedCategories = master.prepare("SELECT b.Server, SUM(a.time)/60/60 FROM record_snapshot AS a JOIN maps AS b ON a.map = b.map WHERE name = ? GROUP BY b.Server ORDER BY SUM(a.time) DESC").all(player)
 
     if(!mostPlayedMaps.length)
         return await interaction.followUp({
